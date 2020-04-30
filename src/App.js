662d6238a2f18a99bcf011d382f149cd522dd79e
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-04-30 16:05:28 -0500

    register api connected to react

--- src/App.js
+++ src/App.js
@@ -2,12 +2,47 @@ import React from 'react';
 import './App.css';
 import ProductsContainer from './ProductsContainer'
 
-function App() {
-  return (
-    <div className="App">
-    <ProductsContainer/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      loggedIn: false,
+      loggedInUserEmail: ''
+
+    }
+  }
+  register = async(registerInfo) =>{
+    const url = process.env.REACT_APP_API_URL + "api/v1/register"
+    try{
+      const registerResponse = await fetch(url,{
+        credentials: 'include',
+        methods: 'POST',
+        body: JSON.stringify(registerInfo),
+        headers:{
+          'content-type': 'application/json'
+        }
+        const registerJson = await registerResponse.json()
+        if(registerResponse.status === 200){
+          this.setState({
+            loggedIn: true,
+            loggedInUserEmail: registerJson.data.email
+          })
+        }
+      })
+      
+    
+    }catch(err){
+      console.log(err)  
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+      <ProductsContainer/>
+      </div>
+    );
+
+  }
 }
 
 export default App;
